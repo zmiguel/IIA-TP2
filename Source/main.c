b9b5ae5d2ec64d7b51a9eaa746d861960753d9e7
ZMiguel Alves <zmiguel.alves@gmail.com>
2018-12-22 18:51:22 +0000

    Update

---
+++ Source/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+
+//#include "vars.h"
+//#include "util.h"
+//#include "util.c"
+
+int main(int argc, char *argv[]){
+    printf("help");
+}
